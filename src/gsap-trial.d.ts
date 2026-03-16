d1c0322383f21b3dc8f739273591bf0cf61bb44f
JagadeeshAjjada <jagadeesh.ajjada493@gmail.com>
2026-03-16 16:51:13 -0700

    GSAP changes making trail version to stable version

--- src/gsap-trial.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'gsap-trial/SplitText' {
-  export class SplitText {
-    chars: Element[];
-    words: Element[];
-    lines: Element[];
-    constructor(target: any, vars?: object);
-    revert(): void;
-  }
-}
